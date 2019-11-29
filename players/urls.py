fcc3eafc75da92428ab8a08e6b36f0c2975e5b93
TaigaTenori <root@localhost>
2019-11-29 15:26:03 +0100

    Added delete character functionality

--- players/urls.py
+++ players/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('view/<str:name>', views.view_character, name='view_character'),
+    path('delete/<str:name>', views.delete_character, name='delete_character'),
 ]
--- players/views.py
+++ players/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, get_object_or_404, reverse
+from django.shortcuts import render, get_object_or_404, reverse, redirect
 from .models import Player
-
+from django.contrib import messages
 from datetime import datetime
-
+from django.contrib.auth.decorators import login_required
+from global_site import urls
 
 def view_character(request, name):
     player = get_object_or_404(Player, name=name)
@@ -11,3 +12,13 @@ def view_character(request, name):
     #TODO: Add a template filter instead
     created = datetime.fromtimestamp(player.created)
     return render(request, 'players/view_character.html', {'player': player, 'created': created})
+
+@login_required
+def delete_character(request, name):
+    character = get_object_or_404(Player, name=name)
+    if character.account_id == request.user.id:
+        character.delete()
+        messages.success(request, "Character " + name + " has been deleted.")
+    else:
+        messages.warning(request, "You can't delete characters you don't own.")
+    return redirect(reverse('account_overview'))
