1fd077512425469e81225562c1d1bd6462de6e1a
TaigaTenori <root@localhost>
2019-11-30 20:42:01 +0100

    Fixed a silly mistake in confirm.js - it would delete a random character instead of the one clicked

--- global_site/static/js/confirm.js
+++ global_site/static/js/confirm.js
@@ -1,6 +1,6 @@
-function deleteConfirm() {
+function deleteConfirm(url) {
     event.preventDefault();
-
+    console.log(url)
     Swal.fire({
       title: 'Are you sure?',
       text: "You won't be able to revert this!",
@@ -11,7 +11,7 @@ function deleteConfirm() {
       confirmButtonText: 'Yes, delete it!'
     }).then((result) => {
       if (result.value) {
-        location.href = $('#delete_character').attr('href');
+        location.href = url;
       }
     })
 }
